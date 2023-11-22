f5aa3f0ecfdf9a74d3c5be781eaafb08e2363037
Stefano <stefe68@gmail.com>
2023-11-22 14:45:49 +0100

    Add Migration, Database created

---
+++ prisma/migrations/20231122133251_create_table_post/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(100) NOT NULL,
+    `slug` VARCHAR(150) NOT NULL,
+    `image` TEXT NULL,
+    `published` BOOLEAN NOT NULL DEFAULT false,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
